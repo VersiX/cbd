6e018cabb79dc9c15811ba12235444294e248ff9
VersiX <yurkatj7@gmail.com>
2020-05-30 12:12:13 +0300

    nav not ready

---
+++ src/components/nav.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './navAssets/nav.css';
+import { ReactComponent as SearchLogo } from './navAssets/SearchIco.svg';
+
+
+const MenuItem = (props) => {
+    return <li className="nav-menu__item"><a href={props.link}>{props.text}</a></li>;
+}
+
+const Search = (props) => {
+    const [isActive, setIsActive] = React.useState(false);
+
+    function ActiveChange(event) {
+        setIsActive(!isActive);
+    }
+
+    return <div className="nav-additional__item" onClick={ActiveChange}>
+        <input className={isActive ? "search__input" : "search__input search--hidden"} />
+
+        <SearchLogo />
+
+        <div className={isActive ? "search__text search--hidden" : "search__text"}> Search</div>
+    </div >;
+}
+
+
+const Menu = (props) => {
+    return <ul className="nav-menu">
+        <MenuItem link="#" text="HOME" />
+        <MenuItem link="#" text="PRODUCTS" />
+        <MenuItem link="#" text="FAQS" />
+        <MenuItem link="#" text="CONTACT US" />
+    </ul>;
+}
+
+const Logo = (props) => {
+    return <div className="nav__logo">CBD</div>;
+}
+
+const Additional = (props) => {
+    return <div className="nav-additional">
+        <button className="nav-additional__item">Log in</button>
+        <Search />
+        <button className="nav-additional__item">Cart</button>
+    </div>;
+}
+
+const Nav = (props) => {
+    return <nav className="nav">
+        <Menu />
+        <Logo />
+        <Additional />
+    </nav>;
+}
+
+
+export default Nav;
