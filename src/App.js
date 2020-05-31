17a51222625f842be05a3ffe4ae8c69e79552f0a
VersiX <yurkatj7@gmail.com>
2020-05-31 19:36:17 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/nav';
+import Intro from './components/intro';
+import About from './components/about';
+
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Intro />
+      <About />
+
     </div>
   );
 }
